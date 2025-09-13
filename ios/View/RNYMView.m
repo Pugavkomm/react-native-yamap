c24fb3cd7ca415822773762cd2c49932dca52b38
Pugavkomm <slavapugavko2@gmail.com>
2025-09-13 00:36:48 -0300

    Revert "Safe add to array and rerender"
    This reverts commit b709d03186400ba4458cdbf9693f12245cf8aa15.

--- ios/View/RNYMView.m
+++ ios/View/RNYMView.m
@@ -803,8 +803,6 @@
         }
         
     }
-    [_reactSubviews addObject:subview];
-    [self setNeedsLayout];
 }
 
 - (void)insertMarkerReactSubview:(UIView<RCTComponent> *) subview atIndex:(NSInteger) atIndex {
